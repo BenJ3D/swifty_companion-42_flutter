a1f86d588f8a931bbd1c714c1abd39ebf5e77dbe
Benjamin DUCROCQ <benjamin@ben3d.fr>
2024-07-16 22:19:42 +0200

    Auth42: step3: token obtenu
    TODO: stocker le token + interceptor http

--- lib/AuthService.dart
+++ lib/AuthService.dart
@@ -1,8 +1,42 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:app_links/app_links.dart';
+import 'package:http/http.dart' as http;
+
+class AuthToken {
+  final String accessToken;
+  final String tokenType;
+  final int expiresIn;
+  final String refreshToken;
+  final String scope;
+  final int createdAt;
+  final int secretValidUntil;
+
+  AuthToken({
+    required this.accessToken,
+    required this.tokenType,
+    required this.expiresIn,
+    required this.refreshToken,
+    required this.scope,
+    required this.createdAt,
+    required this.secretValidUntil,
+  });
+
+  factory AuthToken.fromJson(Map<String, dynamic> json) {
+    return AuthToken(
+      accessToken: json['access_token'],
+      tokenType: json['token_type'],
+      expiresIn: json['expires_in'],
+      refreshToken: json['refresh_token'],
+      scope: json['scope'],
+      createdAt: json['created_at'],
+      secretValidUntil: json['secret_valid_until'],
+    );
+  }
+}
 
 class AuthService {
   late AppLinks _appLinks;
@@ -42,10 +76,34 @@ class AuthService {
     }
   }
 
-  void _handleCode(String code) {
-    // Logique pour gérer le code d'autorisation
-    print('Code d\'autorisation reçu: $code');
-    // Ajoutez ici la logique pour traiter le code (par exemple, échanger le code contre un token)
+  Future<void> _handleCode(String code) async {
+    try {
+      final token = await _exchangeCodeForToken(code);
+      // Utilisez le token comme vous le souhaitez
+      print('Token obtenu: ${token.accessToken}');
+    } catch (e) {
+      print('Erreur lors de l\'obtention du token: $e');
+    }
+  }
+
+  Future<AuthToken> _exchangeCodeForToken(String code) async {
+    final url = Uri.parse('https://api.intra.42.fr/oauth/token');
+    final response = await http.post(
+      url,
+      body: {
+        'grant_type': 'authorization_code',
+        'client_id': dotenv.env['CLIENT_UID'],
+        'client_secret': dotenv.env['CLIENT_SECRET'],
+        'code': code,
+        'redirect_uri': dotenv.env['REDIRECT_URI'],
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return AuthToken.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Échec de l\'obtention du token: ${response.body}');
+    }
   }
 
   Future<void> login() async {
