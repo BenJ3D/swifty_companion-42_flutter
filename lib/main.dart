369ff299b619a8647f4fd99715c6b2af29f644cb
Benjamin DUCROCQ <benjamin@ben3d.fr>
2024-07-16 21:44:12 +0200

    Auth42: step2: recuperation du code Auth: OK

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:async'; // Importation nécessaire pour StreamSubscription
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:app_links/app_links.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
 
   runApp(const MyApp());
@@ -34,11 +37,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late AppLinks _appLinks;
+  StreamSubscription<Uri>? _sub;
+
+  @override
+  void initState() {
+    super.initState();
+    initAppLinks();
+  }
+
+  Future<void> initAppLinks() async {
+    _appLinks = AppLinks();
+
+    // Pour traiter les liens entrants quand l'application est déjà ouverte
+    _sub = _appLinks.uriLinkStream.listen((Uri? uri) {
+      if (uri != null) {
+        _handleIncomingLink(uri);
+      }
+    }, onError: (err) {
+      // Gérer les erreurs ici
+      print('Erreur: $err');
+    });
+
+    // Pour traiter les liens entrants quand l'application démarre via un lien
+    try {
+      final initialUri = await _appLinks.getInitialLink();
+      if (initialUri != null) {
+        _handleIncomingLink(initialUri);
+      }
+    } catch (e) {
+      // Gérer les erreurs ici
+      print('Erreur lors de la récupération du lien initial: $e');
+    }
+  }
+
+  void _handleIncomingLink(Uri uri) {
+    final code = uri.queryParameters['code'];
+    if (code != null) {
+      // Utiliser le code d'autorisation ici
+      print('Code d\'autorisation: $code');
+    }
+  }
+
   Future<void> loginFunction() async {
     final Uri url = Uri.parse(dotenv.env['CLIENT_AUTHORIZE_URL'].toString());
     if (!await launchUrl(url)) {
       throw Exception('Could not launch $url');
     }
+  }
+
+  @override
+  void dispose() {
+    _sub?.cancel();
+    super.dispose();
   }
 
   @override
